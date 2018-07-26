97af972606154c787ed58cb48d1a472b4fb9c111
Your Name <dre.murguido@gmail.com>
2018-07-26 14:06:34 -0400

    Created ItemsPList for persistent data. Reformatted to MVC structure.

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -10,14 +10,15 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    var itemArray = ["Protfolio", "Resume", "Projects", "Design"]
-    let defaults = UserDefaults.standard
+    var itemArray = [Todo]()
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
-            itemArray = items
-        }
+        let newItem = Todo()
+        newItem.title = "New Item"
+        itemArray.append(newItem)
+        loadItems()
     }
     //MARK - Tableview Datasource Methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -26,18 +27,17 @@ class TodoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        cell.textLabel?.text = item.title
+        cell.accessoryType = item.done ? .checkmark : .none
         return cell;
     }
     //MARK - TableView Delegate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 //        print(itemArray[indexPath.row])
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        saveItems()
         
-        if (tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark){
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-                tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
         tableView.deselectRow(at: indexPath, animated: true)
     }
     //MARK - Add New Items
@@ -46,10 +46,10 @@ class TodoListViewController: UITableViewController {
         let alert = UIAlertController(title: "Add New Todoey", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //Added Item
-            self.itemArray.append(textField.text!)
-            
-            self.defaults.set(self.itemArray, forKey: "TodoListArray")
-            self.tableView.reloadData()
+            let newTodo = Todo()
+            newTodo.title = textField.text!
+            self.itemArray.append(newTodo)
+            self.saveItems()
         }
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Add Todo Item"
@@ -60,7 +60,26 @@ class TodoListViewController: UITableViewController {
         present(alert, animated: true, completion: nil);
     }
     
+    func saveItems(){
+        let encoder = PropertyListEncoder()
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        } catch {
+            print("Error Encode item array, \(error)")
+        }
+        tableView.reloadData()
+    }
     
-    
+    func loadItems(){
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+             itemArray = try decoder.decode([Todo].self, from: data)
+            } catch {
+                print("error \(error)")
+            }
+        }
+    }
 }
 
---
+++ Todoey/Models/Todo.swift
@@ -0,0 +1,13 @@
+//
+//  TodoModel.swift
+//  Todoey
+//
+//  Created by andres murguido on 7/26/18.
+//  Copyright © 2018 andres murguido. All rights reserved.
+//
+
+import Foundation
+class Todo: Codable {
+    var title: String = ""
+    var done: Bool = false
+}
